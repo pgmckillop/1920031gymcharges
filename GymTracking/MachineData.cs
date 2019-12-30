aed9093cdb55341464cd89af7eaccc97ef252195
Paul McKillop <pgmckillop@gmail.com>
2019-12-30 21:37:02 +0000

    First commit Scan 3eXS
    MachineData Class added

---
+++ GymTracking/MachineData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * Title:   MachineData
+ * Author:  Paul McKillop
+ * Date:    29 December 2019
+ * Purpose: Data for each machine : Name, Level, Rate
+ */
+
+namespace GymTracking
+{
+    public class MachineData
+    {
+        private string machineName;
+
+        public string MachineName
+        {
+            get { return machineName; }
+            set { machineName = value; }
+        }
+
+        private string level;
+
+        public string Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+
+        private float rate;
+
+        public float Rate
+        {
+            get { return rate; }
+            set { rate = value; }
+        }
+
+
+
+    }
+}
